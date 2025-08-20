e2544daf2b358ce9d7dd6892980ec5e5930c8772
허세진 <heosejin222@gmail.com>
2025-08-20 21:28:21 +0900

    3.1 Overloading

--- index.ts
+++ index.ts
@@ -1,3 +1,20 @@
 type Add = (a:number, b:number) => number;
 
 const add:Add = (a,b) => a+b;
+
+type Config = {
+    path: string,
+    state: object
+}
+
+type Push = {
+    (path:string):void
+    (config:Config):void
+}
+
+const push:Push= (config) => {
+    if(typeof config === "string") {console.log(config)}
+    else {
+        console.log(config.path);
+    }
+}
